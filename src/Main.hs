30ea9560f47cb0c224e82f9d9dfdc7323cac2fcf
ARIA <2329152202@qq.com>
2023-02-09 23:30:19 -0600

    basic cmd options parsing (help, version)
    Using "optparse-applicative"

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,46 @@
 import System.Console.ANSI
+import Options.Applicative
+import Data.Semigroup ((<>))
 import System.IO
+
+data Opts = Opts
+    { optFlag :: !Bool
+    , optVal :: !String
+    }
 
 banner :: IO ()
 banner = do
     setSGR [SetColor Foreground Vivid Green]
     putStrLn "Lumi Programming Language"
-    putStrLn "Version 0.0.1"
-    putStrLn ""
-
-help :: IO ()
-help = do
     setSGR [SetColor Foreground Dull Green]
-    putStrLn "-------------- Manual --------------"
-    putStrLn " 1. Run as Interactive:"
-    putStrLn " 'lumi'"
-    putStrLn " 2. Run as Interpreter:"
-    putStrLn " 'lumi <source file> -args'"
-    putStrLn " 3. Run as Compiler:"
-    putStrLn " 'lumi -c <source file> <output file>'"
-    putStrLn " Help Information:"
-    putStrLn " 'lumi -?' or 'lumi -help'"
     putStrLn ""
 
 main :: IO ()
 main = do
     banner
-    help
+    opts <- execParser optsParser
     putStrLn ""
-    -- TODO: run interactively
+    where
+        -- Use "./lumi --x" ('lumi' is not a command)
+        optsParser = 
+            info
+                (helper <*> versionOption)
+                fullDesc
+        versionOption :: Parser (a -> a)
+        versionOption = infoOption "Version 0.0.1" (long "version" <> help "Show current version")
+        -- https://www.fpcomplete.com/haskell/library/optparse-applicative/
+        -- programOptions :: Parser Opts
+        -- programOptions =
+        --     Opts <$> switch (long "global-flag" <> help "Set a global flag") <*>
+            -- hsubparser lumiREPL
+        -- lumiREPL ::  Mod CommandFields Command
+        -- lumiREPL = 
+        --     command
+        --         "repl"
+        --         (info "Lumi_REPL_Parser_Command" (progDesc "Lumi Interactive Compiler"))
+
+        -- TODO: 
+        -- lumiInterpreter: lumi <source file> -args
+        -- lumiCompiler: lumi -c <source file> <output file>
+        -- and corresponding commands of the 3 options
+
