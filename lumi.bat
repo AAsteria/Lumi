906d50f832cc8e92dfea77bb850daa2efcfa88f0
Yang Zhou <yangz17@illinois.edu>
2023-03-06 05:37:53 -0600

    Finished lumi bat file instructions

--- lumi.bat
+++ lumi.bat
@@ -1,6 +1,14 @@
+@REM Instructions on how to edit this file
+@REM Change the 11th line to "cd /d whatever/location/you/have/for/your/lumi/folder"
+@REM Instructions on how to add the lumi folder into your environment variables:
+@REM 1. Search "environment" in the Taskbar, and click "Edit the system environment variables"
+@REM 2. Under "Advanced", click "Environment Variables"
+@REM 3. Select the "Path" variable under "System Variables", click "Edit"
+@REM 4. Click "New", then copy and paste your Lumi folder's path into system paths
+
 @echo off
-cd /d C:\Users\20445\Desktop\CS 498\Lumi
-call npm.cmd start
-call stack.exe install
-cd /d C:\Users\20445\AppData\Roaming\local\bin
+cd /d %USERPROFILE%\Desktop\CS 498\Lumi
+@REM Ignore for now: "call npm.cmd start"
+call stack install
+cd /d %USERPROFILE%\AppData\Roaming\local\bin
 call lumi.exe
